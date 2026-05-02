405ecdf9811e4e4e9784b83a6ab8cfd9bbd77d27
Antoine Neff <9216777+antoineneff@users.noreply.github.com>
2026-05-02 19:16:20 +0200

    feat(albion-packets): add PhotonEvent derive macro for declarative packet decoding

--- crates/albion-packets/src/lib.rs
+++ crates/albion-packets/src/lib.rs
@@ -46,51 +46,34 @@ pub fn decode_request(params: HashMap<u8, PhotonValue>) -> Option<AlbionOperatio
     let op_type = OperationType(code);
 
     match op_type {
-        OperationType::opGetGameServerByCluster => Some(
-            AlbionOperation::RequestGetGameServerByCluster(OperationGetGameServerByCluster {
-                cluster_id: get_param(&params, 1).unwrap_or_default(),
-            }),
-        ),
-        OperationType::opAuctionGetOffers => Some(AlbionOperation::RequestAuctionGetOffers(
-            OperationAuctionGetOffers {
-                category: get_param(&params, 1).unwrap_or_default(),
-                sub_category: get_param(&params, 2).unwrap_or_default(),
-                quality: get_param(&params, 5).unwrap_or_default(),
-                enchantment: get_param(&params, 6).unwrap_or_default(),
-                enchantment_level: get_param(&params, 10).unwrap_or_default(),
-                item_ids: get_param(&params, 8).unwrap_or_default(),
-                max_results: get_param(&params, 12).unwrap_or_default(),
-                is_ascending_order: get_param(&params, 14).unwrap_or_default(),
-            },
-        )),
-        OperationType::opAuctionGetItemAverageStats => {
-            Some(AlbionOperation::RequestAuctionGetItemAverageStats(
-                OperationAuctionGetItemAverageStats {
-                    item_id: get_param(&params, 1).unwrap_or_default(),
-                },
-            ))
+        OperationType::opGetGameServerByCluster => {
+            OperationGetGameServerByCluster::decode(&params)
+                .map(AlbionOperation::RequestGetGameServerByCluster)
         }
-        OperationType::opGetClusterMapInfo => Some(AlbionOperation::RequestGetClusterMapInfo(
-            OperationGetClusterMapInfo {
-                cluster_id: get_param(&params, 1).unwrap_or_default(),
-            },
-        )),
+        OperationType::opAuctionGetOffers => {
+            OperationAuctionGetOffers::decode(&params)
+                .map(AlbionOperation::RequestAuctionGetOffers)
+        }
+        OperationType::opAuctionGetItemAverageStats => {
+            OperationAuctionGetItemAverageStats::decode(&params)
+                .map(AlbionOperation::RequestAuctionGetItemAverageStats)
+        }
+        OperationType::opGetClusterMapInfo => {
+            OperationGetClusterMapInfo::decode(&params)
+                .map(AlbionOperation::RequestGetClusterMapInfo)
+        }
         OperationType::opGoldMarketGetAverageInfo => {
-            Some(AlbionOperation::RequestGoldMarketGetAverageInfo(
-                OperationGoldMarketGetAverageInfo::default(),
-            ))
+            OperationGoldMarketGetAverageInfo::decode(&params)
+                .map(AlbionOperation::RequestGoldMarketGetAverageInfo)
         }
         OperationType::opRealEstateGetAuctionData => {
-            Some(AlbionOperation::RequestRealEstateGetAuctionData(
-                OperationRealEstateGetAuctionData::default(),
-            ))
+            OperationRealEstateGetAuctionData::decode(&params)
+                .map(AlbionOperation::RequestRealEstateGetAuctionData)
         }
-        OperationType::opRealEstateBidOnAuction => Some(
-            AlbionOperation::RequestRealEstateBidOnAuction(OperationRealEstateBidOnAuction {
-                auction_id: get_param(&params, 1).unwrap_or_default(),
-                bid_amount: get_param(&params, 2).unwrap_or_default(),
-            }),
-        ),
+        OperationType::opRealEstateBidOnAuction => {
+            OperationRealEstateBidOnAuction::decode(&params)
+                .map(AlbionOperation::RequestRealEstateBidOnAuction)
+        }
         _ => None,
     }
 }
@@ -105,79 +88,42 @@ pub fn decode_response(
 
     match op_type {
         OperationType::opJoin => {
-            let character_id = get_param::<Vec<u8>>(&params, 1)
-                .map(|bytes| utils::decode_character_id(&bytes))
-                .unwrap_or_default();
-            let guild_id = get_param::<Vec<u8>>(&params, 56)
-                .map(|bytes| utils::decode_character_id(&bytes))
-                .unwrap_or_default();
-            Some(AlbionOperation::ResponseJoin(OperationJoinResponse {
-                character_id,
-                character_name: get_param(&params, 2).unwrap_or_default(),
-                location: get_param(&params, 8).unwrap_or_default(),
-                guild_id,
-                guild_name: get_param(&params, 58).unwrap_or_default(),
-            }))
+            OperationJoinResponse::decode(&params).map(AlbionOperation::ResponseJoin)
         }
-        OperationType::opAuctionGetOffers => Some(AlbionOperation::ResponseAuctionGetOffers(
-            OperationAuctionGetOffersResponse {
-                market_orders: get_string_array(&params, 0).unwrap_or_default(),
-            },
-        )),
-        OperationType::opAuctionGetRequests => Some(AlbionOperation::ResponseAuctionGetRequests(
-            OperationAuctionGetRequestsResponse {
-                market_orders: get_string_array(&params, 0).unwrap_or_default(),
-            },
-        )),
+        OperationType::opAuctionGetOffers => {
+            OperationAuctionGetOffersResponse::decode(&params)
+                .map(AlbionOperation::ResponseAuctionGetOffers)
+        }
+        OperationType::opAuctionGetRequests => {
+            OperationAuctionGetRequestsResponse::decode(&params)
+                .map(AlbionOperation::ResponseAuctionGetRequests)
+        }
         OperationType::opAuctionGetItemAverageStats => {
-            Some(AlbionOperation::ResponseAuctionGetItemAverageStats(
-                OperationAuctionGetItemAverageStatsResponse {
-                    item_id: get_param(&params, 1).unwrap_or_default(),
-                    average_price: get_param(&params, 2).unwrap_or_default(),
-                },
-            ))
+            OperationAuctionGetItemAverageStatsResponse::decode(&params)
+                .map(AlbionOperation::ResponseAuctionGetItemAverageStats)
         }
-        OperationType::opGetMailInfos => Some(AlbionOperation::ResponseGetMailInfos(
-            OperationGetMailInfosResponse {
-                mail_count: get_param(&params, 1).unwrap_or_default(),
-            },
-        )),
-        OperationType::opReadMail => Some(AlbionOperation::ResponseReadMail(
-            OperationReadMailResponse {
-                mail_id: get_param(&params, 1).unwrap_or_default(),
-                subject: get_param(&params, 2).unwrap_or_default(),
-                body: get_param(&params, 3).unwrap_or_default(),
-            },
-        )),
-        OperationType::opGetClusterMapInfo => Some(AlbionOperation::ResponseGetClusterMapInfo(
-            OperationGetClusterMapInfoResponse {
-                cluster_id: get_param(&params, 1).unwrap_or_default(),
-                cluster_name: get_param(&params, 2).unwrap_or_default(),
-            },
-        )),
+        OperationType::opGetMailInfos => {
+            OperationGetMailInfosResponse::decode(&params)
+                .map(AlbionOperation::ResponseGetMailInfos)
+        }
+        OperationType::opReadMail => {
+            OperationReadMailResponse::decode(&params).map(AlbionOperation::ResponseReadMail)
+        }
+        OperationType::opGetClusterMapInfo => {
+            OperationGetClusterMapInfoResponse::decode(&params)
+                .map(AlbionOperation::ResponseGetClusterMapInfo)
+        }
         OperationType::opGoldMarketGetAverageInfo => {
-            Some(AlbionOperation::ResponseGoldMarketGetAverageInfo(
-                OperationGoldMarketGetAverageInfoResponse {
-                    average_gold_price: get_param(&params, 1).unwrap_or_default(),
-                },
-            ))
+            OperationGoldMarketGetAverageInfoResponse::decode(&params)
+                .map(AlbionOperation::ResponseGoldMarketGetAverageInfo)
         }
         OperationType::opRealEstateGetAuctionData => {
-            Some(AlbionOperation::ResponseRealEstateGetAuctionData(
-                OperationRealEstateGetAuctionDataResponse {
-                    auction_id: get_param(&params, 1).unwrap_or_default(),
-                    starting_bid: get_param(&params, 2).unwrap_or_default(),
-                    buyout_price: get_param(&params, 3).unwrap_or_default(),
-                },
-            ))
+            OperationRealEstateGetAuctionDataResponse::decode(&params)
+                .map(AlbionOperation::ResponseRealEstateGetAuctionData)
         }
         OperationType::opRealEstateBidOnAuction => {
-            Some(AlbionOperation::ResponseRealEstateBidOnAuction(
-                OperationRealEstateBidOnAuctionResponse {
-                    success: get_param(&params, 1).unwrap_or_default(),
-                    auction_id: get_param(&params, 2).unwrap_or_default(),
-                },
-            ))
+            OperationRealEstateBidOnAuctionResponse::decode(&params)
+                .map(AlbionOperation::ResponseRealEstateBidOnAuction)
         }
         _ => None,
     }
@@ -189,79 +135,14 @@ pub fn decode_event(params: HashMap<u8, PhotonValue>) -> Option<AlbionOperation>
 
     match ev_type {
         EventType::evCharacterStats => {
-            let standings = match params.get(&9) {
-                Some(PhotonValue::Dictionary(dict)) => {
-                    let get = |k: i32| -> u64 {
-                        match dict.get(&PhotonValue::Int(k)) {
-                            Some(PhotonValue::Long(v)) => *v as u64,
-                            Some(PhotonValue::Int(v)) => *v as u64,
-                            Some(PhotonValue::Short(v)) => *v as u64,
-                            _ => 0,
-                        }
-                    };
-                    (get(0), get(1), get(2))
-                }
-                _ => (0, 0, 0),
-            };
-            Some(AlbionOperation::EventCharacterStats(EventCharacterStats {
-                player_name: get_param(&params, 1).unwrap_or_default(),
-                guild_name: get_param(&params, 2).unwrap_or_default(),
-                alliance_name: get_param(&params, 4).unwrap_or_default(),
-                profile_description: get_param(&params, 5).unwrap_or_default(),
-                reputation: get_param(&params, 8).unwrap_or_default(),
-                total_fame: get_param(&params, 7).unwrap_or_default(),
-                fame_pvp: get_param(&params, 11).unwrap_or_default(),
-                fame_pve: get_param(&params, 13).unwrap_or_default(),
-                fame_gathering: get_param(&params, 14).unwrap_or_default(),
-                fame_crafting: get_param(&params, 16).unwrap_or_default(),
-                total_kills: get_param(&params, 10).unwrap_or_default(),
-                resources_invested: get_param(&params, 15).unwrap_or_default(),
-                current_rank: get_param(&params, 60).unwrap_or_default(),
-                current_rank_points: get_param(&params, 61).unwrap_or_default(),
-                highest_rank_points: get_param(&params, 62).unwrap_or_default(),
-                standing_brecilien: standings.0,
-                standing_smugglers: standings.1,
-                standing_antiquarian: standings.2,
-                arena_battles_played: get_param(&params, 29).unwrap_or_default(),
-                arena_battles_won: get_param(&params, 30).unwrap_or_default(),
-                crystal_arena_matches_played: get_param(&params, 31).unwrap_or_default(),
-                crystal_arena_matches_won: get_param(&params, 32).unwrap_or_default(),
-                crystal_league_5v5_battles: get_param(&params, 59).unwrap_or_default(),
-                crystal_league_5v5_wins: get_param(&params, 45).unwrap_or_default(),
-                crystal_league_5v5_lethal_battles: get_param(&params, 42).unwrap_or_default(),
-                crystal_league_5v5_lethal_wins: get_param(&params, 43).unwrap_or_default(),
-                crystal_league_20v20_battles: get_param(&params, 46).unwrap_or_default(),
-                crystal_league_20v20_wins: get_param(&params, 47).unwrap_or_default(),
-                crystal_league_kills: get_param(&params, 48).unwrap_or_default(),
-                crystal_league_kill_fame: get_param(&params, 49).unwrap_or_default(),
-                crystal_realm_battles: get_param(&params, 36).unwrap_or_default(),
-                crystal_realm_kills: get_param(&params, 38).unwrap_or_default(),
-                crystal_realm_kill_fame: get_param(&params, 39).unwrap_or_default(),
-                infamy_corrupted: get_param(&params, 52).unwrap_or_default(),
-                infamy_corrupted_highest: get_param(&params, 51).unwrap_or_default(),
-                infamy_2v2_hellgates: get_param(&params, 54).unwrap_or_default(),
-                infamy_2v2_hellgates_highest: get_param(&params, 53).unwrap_or_default(),
-                infamy_5v5_hellgates: get_param(&params, 56).unwrap_or_default(),
-                infamy_5v5_hellgates_highest: get_param(&params, 55).unwrap_or_default(),
-                infamy_10v10_hellgates: get_param(&params, 58).unwrap_or_default(),
-                gvg_kills: get_param(&params, 18).unwrap_or_default(),
-                gvg_fights_participated: get_param(&params, 17).unwrap_or_default(),
-                gvg_fame: get_param(&params, 19).unwrap_or_default(),
-            }))
+            EventCharacterStats::decode(&params).map(AlbionOperation::EventCharacterStats)
         }
-        EventType::evRedZoneWorldMapEvent => Some(AlbionOperation::EventRedZoneWorldMapEvent(
-            EventRedZoneWorldMapEvent {
-                event_time: get_param(&params, 0).unwrap_or_default(),
-                phase: get_param(&params, 1).unwrap_or_default(),
-            },
-        )),
-        EventType::evFullAchievementInfo => Some(AlbionOperation::EventFullAchievementInfo(
-            EventFullAchievementInfo {
-                completed_achievement_ids: get_param(&params, 1).unwrap_or_default(),
-                active_achievement_ids: get_param(&params, 2).unwrap_or_default(),
-                active_achievement_levels: get_param(&params, 3).unwrap_or_default(),
-            },
-        )),
+        EventType::evRedZoneWorldMapEvent => {
+            EventRedZoneWorldMapEvent::decode(&params).map(AlbionOperation::EventRedZoneWorldMapEvent)
+        }
+        EventType::evFullAchievementInfo => {
+            EventFullAchievementInfo::decode(&params).map(AlbionOperation::EventFullAchievementInfo)
+        }
         _ => None,
     }
 }
